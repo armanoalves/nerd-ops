2fee575f4dd5714538200d84fe4afc03a81f5797
renan lima <renangenito@hotmail.com>
2024-06-07 13:15:22 -0300

    Criando a modelo comentario

---
+++ src/migrations/20240607161112-create-comentario.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comentarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      conteudo: {
+        type: Sequelize.STRING
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER
+      },
+      post_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comentarios');
+  }
+};
