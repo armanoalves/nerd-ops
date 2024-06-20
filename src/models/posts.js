5e279700eb2f71159089e2ce1a2e311288f14fda
Sarocy <sarahocy@hotmail.com>
2024-06-20 13:19:47 -0300

    Adição de relacionamentos dos posts

--- src/models/posts.js
+++ src/models/posts.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Posts.belongsTo(models.Usuario, { foreignKey: 'usuario_id' });
+      Posts.hasMany(models.Comentario, { foreignKey: 'post_id' });
     }
   }
   Posts.init({
